2dfb148b15acb1d4718b37c8c3aca4fd468909fa
Dirk Lemmermann <dlemmermann@gmail.com>
2021-06-04 10:36:36 +0200

    Initial work.

--- src/main/java/com/dlsc/jfxcentral/data/pull/Label.java
+++ src/main/java/com/dlsc/jfxcentral/data/pull/Label.java
@@ -26,4 +26,27 @@ public class Label {
     @Expose
     private String description;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
