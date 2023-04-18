aa5b4687d4e1a279170552683cc84487a61bffc4
Dirk Lemmermann <dlemmermann@gmail.com>
2023-04-18 11:42:56 +0200

    Added RSSManager class.

---
+++ src/main/java/com/dlsc/jfxcentral/data/RSSManager.java
@@ -0,0 +1,9 @@
+package com.dlsc.jfxcentral.data;
+
+public class RSSManager {
+
+    public static String createRSS() {
+        DataRepository repository = DataRepository.getInstance();
+        return "";
+    }
+}
