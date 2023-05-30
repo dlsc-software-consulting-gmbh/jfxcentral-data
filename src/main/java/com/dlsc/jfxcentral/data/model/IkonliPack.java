4f03c386d06a691b12d90f0f2e56f6fa78ff5e32
leewyatt <leewyatt7788@gmail.com>
2023-05-30 19:08:26 +0900

    Add Model IkonliPack

---
+++ src/main/java/com/dlsc/jfxcentral/data/model/IkonliPack.java
@@ -0,0 +1,72 @@
+package com.dlsc.jfxcentral.data.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class IkonliPack extends ModelObject {
+    private String module;
+
+    private String title;
+
+    @SerializedName("icon_style")
+    private IconStyle iconStyle;
+
+    private Installing installing;
+
+    @SerializedName("source_url")
+    private String url;
+
+    @SerializedName("font_version")
+    private String fontVersion;
+
+    public IkonliPack() {
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public IconStyle getIconStyle() {
+        return iconStyle;
+    }
+
+    public void setIconStyle(IconStyle iconStyle) {
+        this.iconStyle = iconStyle;
+    }
+
+    public Installing getInstalling() {
+        return installing;
+    }
+
+    public void setInstalling(Installing installing) {
+        this.installing = installing;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getFontVersion() {
+        return fontVersion;
+    }
+
+    public void setFontVersion(String fontVersion) {
+        this.fontVersion = fontVersion;
+    }
+}
+
