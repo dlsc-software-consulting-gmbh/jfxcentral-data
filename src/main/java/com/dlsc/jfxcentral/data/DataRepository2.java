91ec7de782fef01ede97ac8754529ab916995085
floriankirmaier <florian.kirmaier@gmail.com>
2024-07-15 16:22:00 +0400

    quick test on CI

--- src/main/java/com/dlsc/jfxcentral/data/DataRepository2.java
+++ src/main/java/com/dlsc/jfxcentral/data/DataRepository2.java
@@ -687,6 +687,8 @@ public class DataRepository2 {
     private void loadArtifactVersion(String groupId, String artifactId, StringProperty result) {
         HttpURLConnection con = null;
 
+        System.out.println("loadArtifactVersion: " + groupId + " " + artifactId);
+
         try {
             URL url = new URL(MessageFormat.format("https://search.maven.org/solrsearch/select?q=g:{0}+AND+a:{1}", groupId, artifactId));
 
@@ -705,6 +707,8 @@ public class DataRepository2 {
                     in.close();
 
                     QueryResult queryResult = gson.fromJson(content.toString(), QueryResult.class);
+                    String version = queryResult.getResponse().getDocs().get(0).getLatestVersion();
+                    System.out.println("version: " + version);
                     result.set(queryResult.getResponse().getDocs().get(0).getLatestVersion());
                 }
             } else {
